58f63761b0d2267621c8853cfb5bcb4bf758ce13
Luis Rodriguez <luisrodge@outlook.com>
2021-05-18 14:42:22 -0600

    Add initial sections controller

---
+++ app/controllers/sections_controller.rb
@@ -0,0 +1,26 @@
+class SectionsController < ApplicationController
+  before_action :set_section, only: %i[show destroy]
+
+  def show
+    render(json: @section, include: ['rows', 'rows.columns'])
+  end
+
+  def create
+    @section = Section.new(section_params)
+    if @section.save
+      render(json: @section)
+    else
+      render_422(@section.errors)
+    end
+  end
+
+  private
+
+  def section_params
+    params.require(:section).permit(:title, :details, :num_of_cols, :application_id)
+  end
+
+  def set_section
+    @section = Section.find(params[:id])
+  end
+end
--- app/models/section.rb
+++ app/models/section.rb
@@ -3,4 +3,19 @@ class Section < ApplicationRecord
   has_many :rows, dependent: :destroy
   has_many :columns, dependent: :destroy
   has_many :elements, dependent: :destroy
+
+  validates :title, presence: true
+
+  attribute :num_of_cols, :integer
+
+  after_create :create_row_columns
+
+  private
+
+  def create_row_columns
+    row = Row.create(section_id: id)
+    num_of_cols.times do
+      Column.create(row_id: row.id, section_id: id)
+    end
+  end
 end
