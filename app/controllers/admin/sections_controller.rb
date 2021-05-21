2a75054e62650b840dcdf601218277317ff6a896
Luis Rodriguez <luisrodge@outlook.com>
2021-05-20 20:53:20 -0600

    Moved existing controllers to admin namespace

---
+++ app/controllers/admin/sections_controller.rb
@@ -0,0 +1,33 @@
+module Admin
+  class SectionsController < ApplicationController
+    before_action :set_section, only: %i[show destroy]
+
+    def show
+      render(json: @section, include: ['application', 'rows', 'rows.columns', 'rows.columns.input'])
+    end
+
+    def create
+      @section = Section.new(section_params)
+      if @section.save
+        render(json: @section)
+      else
+        render_422(@section.errors)
+      end
+    end
+
+    def destroy
+      @section.destroy
+      head(:ok)
+    end
+
+    private
+
+    def section_params
+      params.require(:section).permit(:title, :details, :num_of_cols, :application_id)
+    end
+
+    def set_section
+      @section = Section.find(params[:id])
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :applications
-  resources :sections
-  resources :rows
-  resources :columns
-  resources :inputs
+  namespace :admin do
+    resources :applications
+    resources :sections
+    resources :rows
+    resources :columns
+    resources :inputs
+  end
 end
