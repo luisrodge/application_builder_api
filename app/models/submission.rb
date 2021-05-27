eb12559d5bbc6d87d36779abb4634ccf5627050f
Luis Rodriguez <luisrodge@outlook.com>
2021-05-26 19:26:52 -0600

    Update create_submission service to add file attachments when present

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -1,4 +1,6 @@
 class Submission < ApplicationRecord
   belongs_to :application
   has_many :submission_sections, dependent: :destroy
+
+  accepts_nested_attributes_for :submission_sections, allow_destroy: true
 end
