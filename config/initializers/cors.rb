bff27756d2f7561cf9db221bbb60fc474c94704c
Luis Rodriguez <luisrodge@outlook.com>
2021-05-27 23:32:43 -0600

    Undo temp commit

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before(0, Rack::Cors) do
   allow do
-    origins '*'
+    origins ENV['CLIENT_ORIGIN']
 
     resource '*', headers: :any, methods: :any
   end
