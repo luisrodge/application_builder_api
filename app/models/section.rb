c0f395a116c228f249ea3a8671b368d4502932d5
Luis Rodriguez <luisrodge@outlook.com>
2021-06-01 22:10:15 -0600

    Fixes

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,17 +4,4 @@ class Section < ApplicationRecord
   has_many :columns, dependent: :destroy
 
   validates :title, presence: true
-
-  attribute :num_of_cols, :integer
-
-  after_create :create_row_columns
-
-  private
-
-  def create_row_columns
-    row = Row.create!(section_id: id)
-    num_of_cols.times do
-      Column.create!(row_id: row.id, section_id: id)
-    end
-  end
 end
